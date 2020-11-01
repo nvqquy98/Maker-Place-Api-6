ff4f6067ddb956cb38632e3d20f0105a4950c541
nvqquy98 <nvqquy98@gmail.com>
2020-11-01 16:19:33 +0700

    Create Model Products and Validate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_052438) do
+ActiveRecord::Schema.define(version: 2020_11_01_091140) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.boolean "published"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
